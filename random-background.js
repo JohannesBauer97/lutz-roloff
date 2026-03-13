dc714941197b373f2bc8fdcdd656024147c59d5b
Johannes Bauer <me@johannes-bauer.de>
2026-03-13 20:36:47 +0100

    Refactor to Vanta.js

--- random-background.js
+++
@@ -1,7 +0,0 @@
-function setRandomBackground() {
-    const randomNumber = Math.floor(Math.random() * 20);
-    const backgroundImagePath = `/assets/backgrounds/background-${randomNumber}.webp`;
-    document.body.style.backgroundImage = `url("${backgroundImagePath}")`;
-}
-
-setRandomBackground();
